6e453722e95fc0cc18735676047265f2593471de
petrkireev <p.kireev@goodt.me>
2021-03-22 18:16:31 +0300

    Project is completed, but the code must be refactored

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,10 +1,7 @@
 package banking;
 import org.sqlite.SQLiteDataSource;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,9 +13,11 @@ public class Main {
     static private String pin;
     static boolean isLogged = false;
     static String dbFile = "./cards.s3db";
+    static Connection con;
     static Statement st;
 
     public static void main(String[] args) {
+
         if (args.length >= 2) {
             if ("-fileName".equals(args[0])) {
                 dbFile = args[1];
@@ -41,6 +40,15 @@ public class Main {
                         printBalance();
                         break;
                     case 2:
+                        addIncome();
+                        break;
+                    case 3:
+                        doTransfer();
+                        break;
+                    case 4:
+                        closeAccount();
+                        break;
+                    case 5:
                         logOut();
                         break;
                 }
@@ -60,6 +68,9 @@ public class Main {
                     case 2:
                         isLogged = logIn();
                         break;
+                    case 7:
+                        getAllInfoFromDb();
+                        break;
                 }
             }
         }
@@ -73,7 +84,10 @@ public class Main {
 
     static void printMenu2() {
         System.out.println("1. Balance");
-        System.out.println("2. Log out");
+        System.out.println("2. Add income");
+        System.out.println("3. Do transfer");
+        System.out.println("4. Close account");
+        System.out.println("5. Log out");
         System.out.println("0. Exit");
     }
 
@@ -120,15 +134,16 @@ public class Main {
         System.out.println("Enter your PIN:");
         String getPin = sc.nextLine();
 
-        balance = getCardDataFromDb(getCard, getPin);
+        boolean success = getCardDataFromDb(getCard, getPin);
 
-        if (balance >= 0) {
+        if (success) {
             System.out.println("You successfully logged in!\n");
-            return true;
+            cardNumber = getCard;
         } else {
             System.out.println("Wrong card number or PIN!\n");
-            return false;
         }
+
+        return success;
     }
 
     static void logOut() {
@@ -139,6 +154,119 @@ public class Main {
     static void printBalance() {
         System.out.printf("Balance: %d\n\n", balance);
     }
+
+    static void addIncome() {
+        System.out.println("Enter income:");
+        long income = sc.nextLong();
+
+        // update balance
+        balance += income;
+        String sql = String.format("UPDATE card SET balance = %d WHERE number LIKE '%s'", balance, cardNumber);
+        System.out.println(sql);
+
+        try {
+            st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Income was added!");
+    }
+
+    static void doTransfer() {
+        System.out.println("Transfer");
+        System.out.println("Enter card number:");
+        String cardRecipient = sc.nextLine();
+
+        if (!isCardCorrect(cardRecipient)) {
+            System.out.println("Probably you made a mistake in the card number. Please try again!");
+        } else {
+            if (!isCardExist(cardRecipient)) {
+                System.out.println("Such a card does not exist.");
+            } else {
+                System.out.println("Enter how much money you want to transfer:");
+                long transfer = sc.nextLong();
+                if (transfer > balance) {
+                    System.out.println("Not enough money!");
+                } else {
+                    doTransferTransaction(cardNumber, cardRecipient, transfer);
+                    System.out.println("Success!");
+                }
+            }
+        }
+
+    }
+
+    static void closeAccount() {
+        String sqlClose = "DELETE FROM card WHERE number = ?";
+
+        try {
+            PreparedStatement stClose = con.prepareStatement(sqlClose);
+            stClose.setString(1, cardNumber);
+            stClose.executeUpdate();
+
+            stClose.close();
+            System.out.println("The account has been closed!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void doTransferTransaction(String sender, String recipient, long amount) {
+        String sqlSender = "UPDATE card SET balance = balance - ? WHERE number = ?";
+        String sqlRecipient = "UPDATE card SET balance = balance + ? WHERE number = ?";
+
+        try {
+            con.setAutoCommit(false);
+
+            PreparedStatement stSender = con.prepareStatement(sqlSender);
+            PreparedStatement stRecipient = con.prepareStatement(sqlRecipient);
+
+            stSender.setLong(1, amount);
+            stSender.setString(2, sender);
+
+            stRecipient.setLong(1, amount);
+            stRecipient.setString(2, recipient);
+
+            stSender.executeUpdate();
+            stRecipient.executeUpdate();
+
+            con.commit();
+            con.setAutoCommit(true);
+
+            stSender.close();
+            stRecipient.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static boolean isCardCorrect(String cardRecipient) {
+        int sum = 0;
+        for (int i = 0; i < cardRecipient.length() - 1; i++) {
+            char c = cardRecipient.charAt(i);
+            int value = Character.getNumericValue(c);
+
+            if (i % 2 == 0) {
+                value *= 2;
+                if (value >= 10) {
+                    value -= 9;
+                }
+            }
+            sum += value;
+        }
+
+        int lastDigit = 0;
+        if (sum % 10 != 0) {
+            lastDigit = ((sum / 10) + 1) * 10 - sum;
+        }
+
+        return Character.getNumericValue(cardRecipient.charAt(cardRecipient.length() - 1)) == lastDigit;
+    }
+
 
     static void addLastDigitByLuhn() {
         int sum = 0;
@@ -161,15 +289,13 @@ public class Main {
         }
 
         cardNumber += lastDigit;
-
-        // System.out.println(cardNumber + " " + sum + " " + lastDigit);
     }
 
     public static void intitializeDb() {
         try {
             SQLiteDataSource dataSource = new SQLiteDataSource();
             dataSource.setUrl("jdbc:sqlite:" + dbFile);
-            Connection con = dataSource.getConnection();
+            con = dataSource.getConnection();
 
             st = con.createStatement();
             st.executeUpdate("CREATE TABLE IF NOT EXISTS card (" +
@@ -188,9 +314,7 @@ public class Main {
     }
 
     public static void insertCardDataIntoDb() {
-        String sql = String.format("INSERT INTO card (id, number, pin) VALUES (%d, %s, %s);", id++, cardNumber, pin);
-        SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl("jdbc:sqlite:" + dbFile);
+        String sql = String.format("INSERT INTO card (id, number, pin) VALUES (%d, '%s', '%s');", id++, cardNumber, pin);
 
         try {
             st.executeUpdate(sql);
@@ -199,15 +323,16 @@ public class Main {
         }
     }
 
-    public static long getCardDataFromDb(String enteredNumber, String enteredPin) {
-        String sql = String.format("SELECT * FROM card WHERE number = %s", enteredNumber);
-        long accountBalance = -1;
+    public static boolean getCardDataFromDb(String enteredNumber, String enteredPin) {
+        String sql = String.format("SELECT * FROM card WHERE number LIKE '%s'", enteredNumber);
+        boolean success = false;
 
         try {
             ResultSet rs = st.executeQuery(sql);
             while (rs.next()) {
                 if (rs.getString("pin").equals(enteredPin)) {
-                    accountBalance = rs.getLong("balance");
+                    balance = rs.getLong("balance");
+                    success = true;
                     break;
                 }
             }
@@ -215,8 +340,23 @@ public class Main {
                 e.printStackTrace();
         }
 
-        return accountBalance;
+        return success;
     }
+
+    public static boolean isCardExist(String enteredNumber) {
+        String sql = String.format("SELECT * FROM card WHERE number LIKE '%s'", enteredNumber);
+        boolean exists = false;
+
+        try {
+            ResultSet rs = st.executeQuery(sql);
+            exists = rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return exists;
+    }
+
 
     public static void closeAll() {
         sc.close();
@@ -226,4 +366,21 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    public static void getAllInfoFromDb() {
+        String sql = "SELECT * FROM card";
+
+        try {
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                System.out.printf("%d\t%s\t%s\t%d\n", rs.getInt("id"), rs.getString("number"), rs.getString("pin"), rs.getLong("balance"));
+            }
+            rs.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
